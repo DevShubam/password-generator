6a3fa40b391ffddd3e79754d509ccb9731bc4f1f
DevShubam <65375709+DevShubam@users.noreply.github.com>
2023-07-23 13:19:55 -0400

    added the seperator and some base js

---
+++ passphrase.js
@@ -0,0 +1,4 @@
+const uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase_chars = "abcdefghijklmnopqrstuvwxyz";
+const numbers_chars = "0123456789";
+const symbols_chars = "!@#%&*()-,.";
