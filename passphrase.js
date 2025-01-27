2c10998ab3cce6f62d2ff843cb121077ed9724fe
DevShubam <65375709+DevShubam@users.noreply.github.com>
2025-01-27 15:31:17 -0500

    Delete passphrase.js

--- passphrase.js
+++
@@ -1,4 +0,0 @@
-const uppercase_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase_chars = "abcdefghijklmnopqrstuvwxyz";
-const numbers_chars = "0123456789";
-const symbols_chars = "!@#%&*()-,.";
